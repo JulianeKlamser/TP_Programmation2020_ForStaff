e6de13a28a7425100bad0130850f446ed970e312
JulianeUta <juliane_klamser@yahoo.de>
2020-04-02 13:37:56 +0200

    my fist C code

---
+++ MyFirstCCode/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, ESPCI! I am learning C. For a new line I need to type this\n :) \n");
+   return 0;
+}
